604a201b7a349b4c33a927aef8d25d2824319b41
kyle mcarthur <teerzo@live.com.au>
2021-01-17 03:20:13 +1100

    fix mobile header, add login route

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -16,10 +16,8 @@ import icon from 'images/icon.png';
 
 export default function Header(props) {
     let location = useLocation();
-    const [lastLocation, setLast] = useState(null);
 
     const { height, width } = useWindowDimensions();
-    const [menu, setMenu] = useState(false);
     const [mobile, setMobile] = useState(false);
 
     useEffect(() => {
@@ -31,21 +29,6 @@ export default function Header(props) {
         }
     }, [width]);
 
-    useEffect(() => {
-        console.log('location', location);
-        console.log('last', lastLocation);
-        if( !lastLocation || lastLocation.pathname !== location.pathname ) {
-           hideMenu();
-        }
-    },[location]);
-
-    function showMenu() {
-        setMenu(true);
-    }
-    function hideMenu() {
-        setMenu(false)
-    }
-
 
     const cmpClasses = cx(
         'header',
@@ -55,39 +38,53 @@ export default function Header(props) {
 
     return (
         <header className={cmpClasses}>
-            <div className="inner">
-                {mobile ? menu ?
-                    <Icon className="close" onClick={hideMenu}>
-                        <FaTimes />
-                    </Icon>
-                    :
-                    <Icon className="open" onClick={showMenu}>
-                        <FaBars />
-                    </Icon>
-                    : null
-                }
-                <div className="title">
-                    <Link path="/">
-                        <div className="flex-row">
-                            {/* <Image src={icon} width={30} height={30} /> */}
-                            <h3> Jungle in my plants </h3>
-                        </div>
-                    </Link>
-                </div>
-                {mobile ? menu ?
-                    null
-                    : null
-                    :
-                    <div className="links">
-                        <Link className="link" noBorder path="/plants" location={location}> Plants </Link>
-                        <Link className="link" noBorder path="/about" location={location}> About </Link>
-                        <div className="grow"> </div>
-                    </div>
-                }
-                <div className="grow"> </div>
+            {mobile ?
+                <MobileHeader location={location} />
+                :
+                <DesktopHeader location={location} />
+            }
+        </header>
+    );
+}
 
+function MobileHeader({ location, ...props }) {
+    const [menu, setMenu] = useState(false);
+    const [lastLocation, setLast] = useState(null);
+
+    useEffect(() => {
+        // console.log('location', location);
+        // console.log('last', lastLocation);
+        if (!lastLocation || lastLocation.pathname !== location.pathname) {
+            hideMenu();
+        }
+    }, [location]);
+
+    function showMenu() {
+        setMenu(true);
+    }
+    function hideMenu() {
+        setMenu(false)
+    }
+    return (
+        <div className="inner">
+            {menu ?
+                <Icon className="close" onClick={hideMenu}>
+                    <FaTimes />
+                </Icon>
+                :
+                <Icon className="open" onClick={showMenu}>
+                    <FaBars />
+                </Icon>
+            }
+            <div className="title">
+                <Link path="/">
+                    <div className="flex-row">
+                        {/* <Image src={icon} width={30} height={30} /> */}
+                        <h3 className="noselect"> Jungle in my plants </h3>
+                    </div>
+                </Link>
             </div>
-            {mobile ? menu ?
+            {menu ?
                 <div className="links-parent">
                     <div className="links">
                         <Link className="link" noBorder path="/plants" location={location}> Plants </Link>
@@ -96,8 +93,83 @@ export default function Header(props) {
                     </div>
                 </div>
                 : null
-                : null
             }
-        </header>
-    );
+        </div>
+    )
 }
+
+function DesktopHeader({ location, ...props }) {
+    const [lastLocation, setLast] = useState(null);
+
+    useEffect(() => {
+        // console.log('location', location);
+        // console.log('last', lastLocation);
+        if (!lastLocation || lastLocation.pathname !== location.pathname) {
+            // hideMenu();
+        }
+    }, [location]);
+
+    return (
+        <div className="flex-row inner">
+            <div className="title">
+                <Link path="/">
+                    <div className="flex-row">
+                        <h3> Jungle in my plants </h3>
+                    </div>
+                </Link>
+            </div>
+            <div className="links">
+                <Link className="link" noBorder path="/plants" location={location}> Plants </Link>
+                <Link className="link" noBorder path="/about" location={location}> About </Link>
+            </div>
+            <div className="grow"> </div>
+            <div className="login">
+                <Link className="link" noBorder path="/login" location={location}> Login </Link>
+            </div>
+        </div>
+    )
+}
+
+    //         {mobile ? menu ?
+    //             <Icon className="close" onClick={hideMenu}>
+    //                 <FaTimes />
+    //             </Icon>
+    //             :
+    //             <Icon className="open" onClick={showMenu}>
+    //                 <FaBars />
+    //             </Icon>
+    //             : null
+    //         }
+    //         <div className="title">
+    //             <Link path="/">
+    //                 <div className="flex-row">
+    //                     {/* <Image src={icon} width={30} height={30} /> */}
+    //                     <h3> Jungle in my plants </h3>
+    //                 </div>
+    //             </Link>
+    //         </div>
+    //         {mobile ? menu ?
+    //             null
+    //             : null
+    //             :
+    //             <div className="links">
+    //                 <Link className="link" noBorder path="/plants" location={location}> Plants </Link>
+    //                 <Link className="link" noBorder path="/about" location={location}> About </Link>
+    //                 <div className="grow"> </div>
+    //             </div>
+    //         }
+    //         <div className="grow"> </div>
+
+    //     </div>
+    // {
+    //     mobile ? menu ?
+    //         <div className="links-parent">
+    //             <div className="links">
+    //                 <Link className="link" noBorder path="/plants" location={location}> Plants </Link>
+    //                 <Link className="link" noBorder path="/about" location={location}> About </Link>
+    //                 <div className="grow"> </div>
+    //             </div>
+    //         </div>
+    //         : null
+    //         : null
+    // }
